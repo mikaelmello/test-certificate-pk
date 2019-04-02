d43140b93f4c56fcf89b7bb3f2286c6d37765a64
Mikael Mello <mikaelm@lacunasoftware.com>
2019-04-02 12:11:45 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestCertificateStorageWithRSA {
+	class Program {
+		static void Main(string[] args) {
+			var certificate = CryptoUtils.CreateSelfSignedCertificate("Test Certificate with RSA Private Key");
+			Console.WriteLine("This is the object that was stored in the X509 store:");
+			Console.WriteLine($"  Name: {certificate.SubjectName.Name}\n  Thumbprint: {certificate.Thumbprint}\n  HasPrivateKey: {certificate.HasPrivateKey}");
+			var retrievedCertificate = CryptoUtils.GetCertificate(certificate.Thumbprint);
+			Console.WriteLine("This is the object that was retrieved from the same X509 store, it should be equal to the one above:");
+			Console.WriteLine($"  Name: {retrievedCertificate.SubjectName.Name}\n  Thumbprint: {retrievedCertificate.Thumbprint}\n  HasPrivateKey: {retrievedCertificate.HasPrivateKey}");
+			Console.ReadLine();
+		}
+	}
+}
